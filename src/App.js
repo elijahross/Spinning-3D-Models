4e090f6a6903206c35334065e6b472afd0595b2d
Elijah Ross <147133625+elijahross@users.noreply.github.com>
2024-01-03 11:37:48 +0000

    Update 7 files

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ export default function App() {
   return (
     <div>
       <Bg />
-      <div className="App">
+      <div className="card">
         <h1>Hello CodeSandbox</h1>
         <h2>Start editing to see some magic happen!</h2>
       </div>
-      <div className="App">
+      <div className="card">
         <h1>Hello CodeSandbox</h1>
         <h2>Start editing to see some magic happen!</h2>
       </div>
-      <div className="App">
+      <div className="card">
         <h1>Hello CodeSandbox</h1>
         <h2>Start editing to see some magic happen!</h2>
       </div>
-      <div className="App">
+      <div className="card">
         <h1>Hello CodeSandbox</h1>
         <h2>Start editing to see some magic happen!</h2>
       </div>
